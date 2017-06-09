1f09d0343df62dc2777166ac776266053019ecdf
Rodri Lopez <rodrilopezypl@gmail.com>
2017-06-09 14:53:02 -0300

    renombrar modelos

--- QnV/qnv/admin.py
+++ QnV/qnv/admin.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import *
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Medicamento)
+admin.site.register(Donacion)
+admin.site.register(Pedir)
+admin.site.register(VerificarIngreso)
+admin.site.register(VerificarRetiro)
