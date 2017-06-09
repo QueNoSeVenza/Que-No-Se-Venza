91fc1d2f6e5baa4fe749bbaa19bb2e4a475d006e
Rodri Lopez <rodrilopezypl@gmail.com>
2017-06-09 14:52:44 -0300

    renombrar modelos

--- QnV/qnv/models.py
+++ QnV/qnv/models.py
@@ -8,37 +8,38 @@ from django.contrib import admin
 # Create your models here.
 
 class Medicamento(models.Model):
+    nombre = models.CharField(max_length=60)
+    concentracion_gramos = models.CharField(max_length=60)
+    laboratorio = models.CharField(max_length=60)
+    droga = models.CharField(max_length=60)
     FUNCION_FARMACEUTICA = (
         ('Analgesico', 'Analgesico'),
         ('Androgenos', 'Androgenos'),
     )
     funcion = models.CharField(max_length=60, choices=FUNCION_FARMACEUTICA, default="Null")
-    PRESCRIPCION_MEDICA = (
-        ('Con Receta','Con Receta'),
-        ('Sin Receta','Sin Receta'),
-    )
-    prescripcion = models.CharField(max_length=60, choices=PRESCRIPCION_MEDICA, default=0)
+    prescripcion = models.BooleanField()
     TIPO = (
         ('Pastillas', 'Pastillas'),
         ('Jarabe', 'Jarabe'),
         ('Gotas', 'Gotas'),
     )
-    nombre = models.CharField(default = '',max_length=60)
     tipo = models.CharField(max_length=60, choices=TIPO, default=0)
-    concentracion_gramos = models.CharField(max_length=60)
-    cantidad = models.CharField(max_length=60)
-    laboratorio = models.CharField(max_length=60)
-    fecha_vencimiento = models.DateField()
-    stock = models.BooleanField(default=False)
+    #cantidad = models.CharField(max_length=60)
    
     def __unicode__(self):
         return self.nombre
 
-
 class Donacion(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     medicamento = models.ForeignKey(Medicamento, on_delete=models.CASCADE)
-    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cantidad = models.CharField(max_length=60)
+    laboratorio = models.CharField(max_length=60)
+    fecha_vencimiento = models.DateField()
+    stock = models.CharField(max_length=60, default="En Espera")
+    
+    def __unicode__(self):
+        return self.nombre
+    
 
 class Pedir(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -50,7 +51,7 @@ class VerificarIngreso(models.Model):
     medicamento = models.ForeignKey(Medicamento, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     
-class VerificarRestiro(models.Model):
+class VerificarRetiro(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     medicamento = models.ForeignKey(Medicamento, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
