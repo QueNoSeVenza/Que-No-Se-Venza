ab5df4957f2e5f3ffbe0e2988ed20385b40a250d
Rodri Lopez <rodrilopezypl@gmail.com>
2017-06-12 07:57:17 -0300

    Funcion donar

--- QnV/donar/views.py
+++ QnV/donar/views.py
@@ -19,28 +19,52 @@ def donar(request):
         m_fecha_vencimiento = request.POST['m_fecha_vencimiento']
         m_tipo = request.POST['m_tipo']
         m_prescripcion = request.POST['m_prescripcion']
+        m_droga = request.POST['m_droga']
         author = request.user
-        medicamento_save = Medicamento(prescripcion=m_prescripcion,
-                                       nombre=m_nombre,
-                                       tipo=m_tipo,
-                                       concentracion_gramos=m_concentracion_gramos,
-                                       cantidad=m_cantidad,
-                                       laboratorio=m_laboratorio,
-                                       fecha_vencimiento=m_fecha_vencimiento)
-        medicamento_save.save()
-        medicamento_id_objeto = Medicamento.objects.get(id=medicamento_save.id)
-        donacion_save = Donacion(user=author,
-                                medicamento=medicamento_id_objeto)
-        donacion_save.save()
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        
+        #comparar medicamento con nombre, concentracion y/o (opcional) laboratorio
+        medicamneto_tabla = Medicamento.objects.get(nombre=m_nombre, 
+                                                    concentracion_gramos=m_concentracion_gramos,
+                                                    laboratorio=m_laboratorio)
+        
+        #if exist:
+        if medicamento is not None:
+            #crear donacio y pasar objeto medicamneto a la tabla donacion
+            donacion_save = Donacion(medicamento=medicamneto_tabla
+                                     user=author,
+                                     cantidad=m_cantidad,
+                                     fecha_vencimiento=m_fecha_vencimiento)
+            donacion_save.save()
+            return redirect('/thanks')
+        #else:
+        else:
+            #crer objeto en tabla medicamneto y crear tabla donacion
+            medicamento_nuevo = Medicamento(nombre=m_nombre,
+                                           concentracion_gramos=m_concentracion_gramos,
+                                           laboratorio=m_laboratorio,
+                                           droga=m_droga,
+                                           prescripcion=m_prescripcion,
+                                           tipo=m_tipo)
+            
+            id_medicamento_nuevo = Medicamento.objects.get(id=medicamento_nuevo.id)
+            
+            donacion_nueva = Donacion(medicamento=id_medicamento_nuevo
+                                     user=author,
+                                     cantidad=m_cantidad,
+                                     fecha_vencimiento=m_fecha_vencimiento)
+            
+            medicamento_nuevo.save()
+            donacion_nueva.save()
+            return redirect('/thanks')
 
 
 def pedir(request):
     if 'POST' in request.method:
         medicamento_pedido = request.POST['medicamento_pedido']
         author = request.user
+        medicamento_objeto = Medicamento.objects.get(nombre=medicamento_pedido)
         pedir_save = Pedir(user=author,
-                          medicamento=medicamento_pedido)
+                          medicamento=medicamento_objeto)
         pedir_save.save()
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
