f754426fc08c4d11520f3c229701533f7f74fe40
Rodri Lopez <rodrilopezypl@gmail.com>
2017-06-13 08:02:28 -0300

    Modifique la funcion de donar y agregue 2 funciones mas

--- QnV/donar/views.py
+++ QnV/donar/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from qnv.models import *
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect,HttpResponse
 from django.template import loader
 
@@ -18,46 +19,20 @@ def donar(request):
         m_laboratorio = request.POST['m_laboratorio']
         m_fecha_vencimiento = request.POST['m_fecha_vencimiento']
         m_tipo = request.POST['m_tipo']
-        m_prescripcion = request.POST['m_prescripcion']
         m_droga = request.POST['m_droga']
         author = request.user
         
-        #comparar medicamento con nombre, concentracion y/o (opcional) laboratorio
-        medicamneto_tabla = Medicamento.objects.get(nombre=m_nombre,
-                                                    concentracion_gramos=m_concentracion_gramos,
-                                                    laboratorio=m_laboratorio)
+        arry = [m_nombre,m_concentracion_gramos,m_cantidad,m_laboratorio,m_fecha_vencimiento,m_tipo,m_droga]
         
-        #if exist:
-        if medicamento is not None:
-            #crear donacio y pasar objeto medicamneto a la tabla donacion
-            donacion_save = Donacion(medicamento=medicamneto_tabla,
-                                    user=author,
-                                    cantidad=m_cantidad,
-                                    fecha_vencimiento=m_fecha_vencimiento)
-            donacion_save.save()
-            return redirect('/thanks')
-        #else:
+        if Medicamento.objects.filter(nombre=arry[0], concentracion_gramos=arry[1], laboratorio=arry[3]).exists():
+            print "if"
+            medicamento_guardado = Medicamento.objects.get(nombre=arry[0], concentracion_gramos=arry[1], laboratorio=arry[3])
+            guardarDonacion(request, arry, medicamento_guardado)
         else:
-            #crer objeto en tabla medicamneto y crear tabla donacion
-            medicamento_nuevo = Medicamento(nombre=m_nombre,
-                                           concentracion_gramos=m_concentracion_gramos,
-                                           laboratorio=m_laboratorio,
-                                           droga=m_droga,
-                                           prescripcion=m_prescripcion,
-                                           tipo=m_tipo)
+            print "else"
+            guardarMedicamento(request, arry)
             
-            id_medicamento_nuevo = Medicamento.objects.get(id=medicamento_nuevo.id)
             
-            donacion_nueva = Donacion(medicamento=id_medicamento_nuevo,
-                                     user=author,
-                                     cantidad=m_cantidad,
-                                     fecha_vencimiento=m_fecha_vencimiento)
-            
-            medicamento_nuevo.save()
-            donacion_nueva.save()
-            return redirect('/thanks')
-
-
 def pedir(request):
     if 'POST' in request.method:
         medicamento_pedido = request.POST['medicamento_pedido']
@@ -75,3 +50,24 @@ def thanks(request):
 		'thanks.html',
 		{}
 )
+
+
+def guardarDonacion(request, arry, medicamento_guardado):
+    donacion = Donacion(medicamento=medicamento_guardado,
+                            user=request.user,
+                            cantidad=arry[2],
+                            fecha_vencimiento=arry[4])
+    donacion.save()
+    return redirect('/donar_index')
+
+
+def guardarMedicamento(request, arry):
+    medicamento = Medicamento(nombre=arry[0],
+                            concentracion_gramos=arry[1],
+                            laboratorio=arry[3],
+                            droga=arry[6],
+                            tipo=arry[5])
+    medicamento.save()
+    medicamento_guardado = Medicamento.objects.get(id=medicamento.id)
+    guardarDonacion(request, arry, medicamento_guardado)
+    return redirect('/donar_index')
