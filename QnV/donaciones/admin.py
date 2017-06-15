682cfd11541db7f8f87d5ac9fcf6eb2e612c7f29
Rodri Lopez <rodrilopezypl@gmail.com>
2017-06-15 09:55:24 -0300

    Añadi la classe MedicamentoDonado

--- QnV/donaciones/admin.py
+++ QnV/donaciones/admin.py
@@ -3,5 +3,6 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Medicamento)
+admin.site.register(MedicamentoDonado)
 admin.site.register(Donacion)
 admin.site.register(Pedir)
