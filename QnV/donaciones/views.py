53cddbb53311d285278fad8b8a296f2c9511ce3d
VictorCesarM <victor.cesar.m@gmail.com>
2017-11-07 10:57:45 -0300

    Agregado upercase a verificar_medicamento

--- QnV/donaciones/views.py
+++ QnV/donaciones/views.py
@@ -119,11 +119,12 @@ def donar(request):
 
 def validate_medicamento(request):
     nombre = request.GET.get('nombre', None)
+    print(nombre)
     concentracion_gramos = request.GET.get('concentracion', None)
     print(concentracion_gramos)
     print(Medicamento.objects.filter(nombre=nombre.upper(),concentracion_gramos=concentracion_gramos))
     data = {
-        'exists': Medicamento.objects.filter(nombre=nombre,concentracion_gramos=concentracion_gramos).exists()
+        'exists': Medicamento.objects.filter(nombre=nombre.upper(),concentracion_gramos=concentracion_gramos).exists()
     }
     return JsonResponse(data)
 
